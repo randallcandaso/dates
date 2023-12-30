c6c5e3b50b497f8fe2296a7b21c7f69f24c05f16
randallcandaso <110688328+randallcandaso@users.noreply.github.com>
2023-12-30 15:36:36 -0700

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,15 +1,3 @@
-'''
-
-File: dates.py
-Author: Randall Candaso
-Course/Section: CSC 120-002
-Purpose: This program takes an inputted file that contains
-specific dates and events. Depending on what the file asks,
-the program is able to store or return events for a specific date
-inputted.
-
-'''
-
 import sys
 
 class Date:
